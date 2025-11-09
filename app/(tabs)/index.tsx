f27da94213448320d0db750309a8a55c5f8f3271
seong5 <greenbi0852@gmail.com>
2025-11-10 02:22:15 +0900

    active-80 으로 터치 시 색상 변환 적용

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import Button from '@/components/Button'
+import React from 'react'
 import { Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
@@ -6,7 +7,7 @@ export default function HomeScreen() {
   return (
     <SafeAreaView>
       <Text>테스트</Text>
-      <Button label="버튼" />
+      <Button label="버튼" onPress={() => {}} />
     </SafeAreaView>
   )
 }
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
-import { Pressable, Text } from 'react-native'
+import { Pressable, PressableProps, Text } from 'react-native'
 
-interface ButtonProps {
+interface ButtonProps extends PressableProps {
   label: string
   size?: 'md' | 'lg'
   variant?: 'primary'
 }
 
-export default function Button({ label, size = 'lg', variant = 'primary' }: ButtonProps) {
+export default function Button({ label, size = 'lg', variant = 'primary', ...props }: ButtonProps) {
   const sizeClass = size === 'lg' ? 'w-full h-11' : 'px-4 py-2'
   const variantClass = variant === 'primary' ? 'bg-[#b0a0e3]' : ''
 
   return (
-    <Pressable className={`rounded-lg justify-center items-center ${sizeClass} ${variantClass}`}>
+    <Pressable
+      {...props}
+      className={`
+        rounded-lg justify-center items-center 
+        ${sizeClass} 
+        ${variantClass}
+        active:opacity-80
+      `}
+    >
       <Text className="text-[#eeeeee] text-[16px] font-semibold">{label}</Text>
     </Pressable>
   )
