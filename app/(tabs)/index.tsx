14b7ac56d60c9847a901b0a33b0f9ad0f3bf500f
seong5 <greenbi0852@gmail.com>
2025-11-10 16:19:31 +0900

    피드카드 컴포넌트 UI 틀 구성

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,11 @@
+import FeedCard from '@/components/FeedCard'
 import React from 'react'
-import { Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
-      <Text>홈화면</Text>
+      <FeedCard />
     </SafeAreaView>
   )
 }
--- components/FeedCard.tsx
+++ components/FeedCard.tsx
@@ -1,5 +1,67 @@
-import { View } from 'react-native'
+import { colors } from '@/constants/colors'
+import Feather from '@expo/vector-icons/Feather'
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons'
+import Octicons from '@expo/vector-icons/Octicons'
+import React from 'react'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 
 export default function FeedCard() {
-  return <View></View>
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.title}>제목</Text>
+        <Text style={styles.description}>내용</Text>
+      </View>
+      <View style={styles.menuContent}>
+        <Pressable style={styles.menu}>
+          <Feather name="heart" size={22} color="black" />
+          <Text style={styles.menuNumber}>2</Text>
+        </Pressable>
+        <Pressable style={styles.menu}>
+          <MaterialCommunityIcons name="message-reply-outline" size={22} color="black" />
+          <Text style={styles.menuNumber}>2</Text>
+        </Pressable>
+        <Pressable style={styles.menu}>
+          <Octicons name="share" size={22} color="black" />
+        </Pressable>
+      </View>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.WHITE,
+  },
+  content: {
+    padding: 16,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: colors.BLACK,
+    marginVertical: 8,
+  },
+  description: {
+    fontSize: 16,
+    color: colors.BLACK,
+    marginBottom: 14,
+  },
+  menuContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    borderTopColor: colors.GRAY_100,
+    borderTopWidth: StyleSheet.hairlineWidth,
+  },
+  menu: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 12,
+    gap: 3,
+  },
+  menuNumber: {
+    fontSize: 14,
+  },
+})
